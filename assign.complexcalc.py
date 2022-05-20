c0926f3ec6f36589a27cd9eeec0794437048e5d6
Sitatijerry <105769901+Sitatijerry@users.noreply.github.com>
2022-05-20 11:57:30 +0300

    Add files via upload

---
+++ assign.complexcalc.py
@@ -0,0 +1,9 @@
+x=float(input("insert first no"))
+op = input("insert operator")
+y= float(input("insert second no"))
+if op=="*":
+    print(x*y)
+if op=="+":
+    print(x+y)
+if op=="-":
+    print(x+y)
