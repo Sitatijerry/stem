fc4eadbddd45fe5d55a65af796aa8062dc5e2285
Sitatijerry <105769901+Sitatijerry@users.noreply.github.com>
2022-05-28 12:02:49 +0300

    Delete assign.complexcalc.py

--- assign.complexcalc.py
+++
@@ -1,9 +0,0 @@
-x=float(input("insert first no"))
-op = input("insert operator")
-y= float(input("insert second no"))
-if op=="*":
-    print(x*y)
-if op=="+":
-    print(x+y)
-if op=="-":
-    print(x+y)
